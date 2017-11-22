d5d04c059e78dc613eb034608704f0a82cfa5745
Rodrigo Nascimento de Carvalho <rodrigonc@ciandt.com>
2017-11-22 21:26:45 -0200

    Adding part 02 exercise solution.

--- src/part02.rs
+++ src/part02.rs
@@ -11,6 +11,7 @@ pub enum SomethingOrNothing<T>  {
 // Instead of writing out all the variants, we can also just import them all at once.
 pub use self::SomethingOrNothing::*;
 type NumberOrNothing = SomethingOrNothing<i32>;
+type FloatOrNothing = SomethingOrNothing<f32>;
 
 // ## Generic `impl`, Static functions
 // Inside an `impl`, `Self` refers to the type we are implementing things for. Here, it is
@@ -66,15 +67,40 @@ impl NumberOrNothing {
         };
     }
 }
-
 // Now we are ready to run our new code. Remember to change `main.rs` appropriately.
 fn read_vec() -> Vec<i32> {
     vec![18,5,7,3,9,27]
 }
+
+//EX 2.01 Minimum implementation for f32
+fn read_vec_float() -> Vec<f32> {
+    vec![18.5,5.1,7.2,3.32,9.321,27.11]
+}
+
+impl FloatOrNothing {
+    pub fn print(self) {
+        match self {
+            Nothing => println!("The float number is: <nothing>"),
+            Something(n) => println!("The float number is: {}", n)
+        }
+    }
+}
+
+impl Minimum for f32 {
+    fn min(self, b: Self) -> Self {
+        if self < b { self } else { b }
+    }
+}
+
 pub fn main() {
     let vec = read_vec();
     let min = vec_min(vec);
     min.print();
+
+    //f32 variant for EX 2.01
+    let float_vec = read_vec_float();
+    let min_float = vec_min(float_vec);
+    min_float.print();
 }
 
 
